c7c3b85ac160411d9f49bb11e960403721ab4e50
sohamkhotpal <100275430+sohamkhotpal@users.noreply.github.com>
2022-02-23 16:20:42 +0530

    Add files via upload

---
+++ Assignment/Assignment1/home.php
@@ -0,0 +1,13 @@
+<?php
+session_start();
+'&nbsp'
+;
+echo "<h1> welcome &nbsp" .$_SESSION['name']."</h1>";
+
+echo "<br><a href='logout.php'><input type=button value=logout name=logout></a>";
+	 
+
+
+
+?>
+	
